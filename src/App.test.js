b71eb3889d5bf6f1bd49c3bb8d3780a5e3bd7272
levelopers <youcancallmeallen@gmail.com>
2019-05-29 21:58:03 -0400

    test practice

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import App from './App';
-import { shallow } from 'enzyme';
+import { shallow ,mount} from 'enzyme';
 import { findByTestAtrr, testStore } from './Utils';
+import { initalState } from './redux/reducers/posts/reducer'
 import React from 'react';
 
-const setUp = (initialState = {}) => {
+
+const setUp = (initialState = { posts: initalState }) => {
   const store = testStore(initialState);
   const wrapper = shallow(<App store={store} />).childAt(0).dive();
   return wrapper;
@@ -12,32 +14,27 @@ const setUp = (initialState = {}) => {
 describe('App Component', () => {
 
   let wrapper;
+  let SharedButton
+  let AppInstance
   beforeEach(() => {
-    const initialState = {
-      posts: [{
-        title: 'Example title 1',
-        body: 'Some text'
-      }, {
-        title: 'Example title 2',
-        body: 'Some text'
-      }, {
-        title: 'Example title 3',
-        body: 'Some text'
-      }]
-    }
-    wrapper = setUp(initialState);
+    wrapper = setUp();
+    AppInstance = wrapper.instance()
+    SharedButton = findByTestAtrr(wrapper, 'sharedButton').childAt(0).dive()
   });
+
   it('Should render without errors', () => {
     const component = findByTestAtrr(wrapper, 'appComponent');
     expect(component.length).toBe(1);
   });
+
+  it('click <SharedButton> should hide the button', () => {
+    const btn = findByTestAtrr(SharedButton, 'buttonComponent')
+    expect(btn.length).toBe(1)
+    btn.simulate('click')
+    expect(AppInstance.state.hideBtn).toBeTruthy()
+    expect(findByTestAtrr(wrapper, 'sharedButton').length).toBe(0)
+  })
 })
 
 
-function add(x, y) {
-  return x + y
-}
 
-test('add', () => {
-  expect(add(1, 2)).toBe(3)
-})
--- src/redux/reducers/posts/posts.test.js
+++ src/redux/reducers/posts/posts.test.js
@@ -1,16 +1,19 @@
-import { types } from '../../actions/types';
-import postsReducer from './reducer';
+import { GET_POSTS } from '../../actions';
+import postsReducer,{initalState} from './reducer';
+
+
 describe('Posts Reducer', () => {
   it('Should return default state', () => {
     const newState = postsReducer(undefined, {});
-    expect(newState).toEqual([]);
+    expect(newState).toEqual(initalState);
   });
+
   it('Should return new state if receiving type', () => {
     const posts = [{ title: 'Test 1' }, { title: 'Test 2' }, { title: 'Test 3' }];
     const newState = postsReducer(undefined, {
-      type: types.GET_POSTS,
+      type: GET_POSTS.SUCCESS,
       payload: posts
     });
-    expect(newState).toEqual(posts);
+    expect(newState).toEqual({...initalState,posts:posts});
   });
 });
